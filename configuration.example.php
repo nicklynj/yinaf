d4fc8bd0966a3e001ee92b58807bb96160b878b1
Nick Nicklyn <nnicklyn@orsinc.com>
2015-03-18 16:25:34 -0400

    config

---
+++ configuration.example.php
@@ -0,0 +1,8 @@
+<?php
+
+abstract class configuration {
+  public static $database_username = 'user';
+  public static $database_password = 'password';
+  public static $database_host = 'example.com';
+  public static $database_name = 'example';
+}

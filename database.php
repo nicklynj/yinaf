8cd4b433e4a3eaf740e57e719afc8f353c3047a9
Nick Nicklyn <nnicklyn@orsinc.com>
2015-04-23 16:54:29 -0400

    allow create with zero attributes

--- database.php
+++ database.php
@@ -216,16 +216,16 @@ class database extends mysqli {
     $default_row = $this->get_default_row($table);
     foreach ($attributes_array_indexed as $index => &$attributes_array_index) {
       $this->query('insert into ' . $this->word($table) . ' ' . 
-        ' (' . implode(',', array_map(
+        ' (' . ($attributes_array_index ? implode(',', array_map(
             array($this, 'word'), 
             array_fill(0, count($attributes_array_index[0]), $table),
             array_keys($attributes_array_index[0])
-        )) . ') values ' .
-        implode(',', array_map(
+        )) : '') . ') values ' .
+        implode(',', $attributes_array_index ? array_map(
           array($this, 'escape_attributes'), 
           array_fill(0, count($attributes_array_index), $table),
           $attributes_array_index
-        ))
+        ) : '()')
       );
       $last_insert_id = $this->insert_id;
       for ($i = 0; isset($attributes_array_index[$i]); ++$i) {

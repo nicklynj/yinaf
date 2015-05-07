2532417aaee461de24788744dda2211591c3ccf5
Nick Nicklyn <nnicklyn@orsinc.com>
2015-05-07 12:13:17 -0400

    temporary crud->search

--- crud.php
+++ crud.php
@@ -57,4 +57,37 @@ class crud extends authenticated {
       ($this->dictionary ? array() : $this->get_root())
     );
   }
+
+  public function search($str) { // [todo:remove this]
+    
+    $queries = array();
+    
+    $queries[] = 'select * from `'.$this->class_name.'` where code like "%'.$this->database->real_escape_string($str).'%"';
+    $queries[] = 'select * from `'.$this->class_name.'` where name like "%'.$this->database->real_escape_string($str).'%"';
+    
+    $results = array();
+    
+    foreach ($queries as $query) {
+      $result = $this->database->query($query . ' limit 100');
+      while ($row = $result->fetch_assoc()) {
+        $results[] = $row;
+      }
+      $results = array_values(array_intersect_key($results, array_unique(array_map('serialize', $results))));
+      if (count($results) >= 100) {
+        break;
+      }
+    }
+    
+    array_multisort(
+      array_column($results, 'code'),
+      SORT_ASC,
+      array_column($results, 'name'),
+      SORT_ASC,
+      $results
+    );
+    
+    return $results;
+    
+  }
+  
 }
